d1e765987c59d89f91667ad92bddc7495cccb425
Alejo-1208 <accruz@hotmail.es>
2023-10-11 17:08:32 -0500

    Subida del proyecto
    Se sube el proyecto al repositorio

---
+++ Source/CursoArkde/CursoArkdeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CursoArkdeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CURSOARKDE_API ACursoArkdeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
